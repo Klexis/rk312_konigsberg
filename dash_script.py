ce0a44f75fdeb17166364c7772a5bc8733c30614
Klexis <51380122+Klexis@users.noreply.github.com>
2020-08-11 13:28:47 +0530

    Update dash_script.py

--- dash_script.py
+++ dash_script.py
@@ -937,4 +937,4 @@ app.callback(
 ########################################################## Run Server ##########################################################
 server=app.server
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0')
+    app.run_server(debug=False, host='0.0.0.0')
